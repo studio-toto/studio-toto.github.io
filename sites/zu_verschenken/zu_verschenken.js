1e5128977d08e49aebeceb7f02412afc7bf14b07
lvitters <luvit023@gmail.com>
2021-05-26 18:17:30 +0200

    Ich wisch hier nur

--- sites/zu_verschenken/zu_verschenken.js
+++ sites/zu_verschenken/zu_verschenken.js
@@ -1,65 +1,52 @@
+var descriptionList = [ "description lost",
+                        "Neustadt",
+                        "Sehr großes Bild 1,20x1,20m angzugeben, Acryl auf Leinwand, Viertel, bitte PN",
+                        "Travel Makeup Brushes von Ecotools in Hemelingen zu verschenken",
+                        "In der Hohentorsheerstraße gefunden, wurde wohl von einer Familie verloren. Bitte weiterleiten und teilen. Bei mir per PN melden, wenn Besitzer gefunden wurde!",
+                        "Kindertoilettensitz in der Neustadt.",
+                        "Bild Ostsee Blick auf Hiddensee , Leinwand",
+                        "RESERVIERT Neu, zu verschenken im Schnoor",
+                        "Hat jemand verwendung für filmdosen? In der Neustadt RESERVIERT",
+                        "A3 Zeichenblöcke zu verschenken - liegen draußen in der hemelinger Straße vor nr 68",
+                        "Kinderteller zu verschenkeb",
+                        "no description",
+                        "kinder neustadt",
+                        "eine Zauberbox (ab 6 Jahren, vollständig, gut erhalten",
+                        "Utrusta backofen halterung (neu, wurde falsch geliefert)",
+                        "Wanduhr.... funktioniert leider nicht....",
+                        "Bremen Schwachhausen....",
+                        "Schwachhausen....",
+                        "Silikon Backform Reserviert",
+                        "Spiegel Massiv Eisen Durchmesser: 62 cm",
+                        "no description",
+                        "Sprechblase die mit einem abwischbaren Stift (whiteboard maker) beschriftet werden kann. Den Stift hab ich nicht mehr",
+                        "Große haarklammer und zopfband im Viertel RESERVIERT",
+                        "Jemand interessiert? Kann CD, USB und SD Karte. Kabel fehlt. Ist aber auch mit Batterien zu betreiben. Weiß leider nicht, ob der noch geht.",
+                        "Kerzenständer zu verschenken in Mitte. - reserviert",
+                        "Zwei einwandfreie Stühle. Abzuholen in der Neustadt! Reserviert",
+                        "In der Neustadt",
+                        "Mandoline (?) in der Neustadt reserviert",
+                        "description lost",
+                        "description lost",
+                        "Selbstverständlich unbenutzt. Nur ein mal geöffnet, um zu prüfen, ob dieses auf meine Maschine passt. Schwachhausen",
+                        "Kratzbaumelement: Hängematte für Kitten oder kleine Katzen. Hat einen Riss, kann aber repariert werden oder neu bezogen werden. Müsste aber ein Kitten auch so aushalten :) Neustadt",
+                        "Hallo liebe Leute habe eine große halal Geflügelwurst 900g abzugeben bei der Ritterhuder Straße 1 in Gröpelingen bitte PN",
+                        "I’m Viertel",
+                        "[Reserviert] Plüschiges Sparschweinchen in Schwachhausen abzugeben.",
+                        "Buntstifte in der Bahnhofsvorstadt zu verschenken",
+                        "Stuhl abzugeben im Viertel :)",
+                        "Funktioniert. Sound halt Werbe- Geschenk-entsprechend. Schwachhausen  RESERVIERT",
+                        "Tauscht jemand diese nagelneue Gugelhupfform gegen eine Tarte-Form (kann gerne schon alt sein, mag keine neuen Sachen). PN Büdde",
+                        "no description",
+                        "Zu verschenken in sebaldsbrück",
+                        "Klo Bär 🤣 in der Neustadt" ]
+
+//for rainbow text
 Splitting();
-
-function changeText(value) {
-    var description = $("#description");
-    var text = "";
-    console.log(description.text());
-
-    var descriptionList = [ "description lost",
-                            "Neustadt",
-                            "Sehr großes Bild 1,20x1,20m angzugeben, Acryl auf Leinwand, Viertel, bitte PN",
-                            "Travel Makeup Brushes von Ecotools in Hemelingen zu verschenken",
-                            "In der Hohentorsheerstraße gefunden, wurde wohl von einer Familie verloren. Bitte weiterleiten und teilen. Bei mir per PN melden, wenn Besitzer gefunden wurde!",
-                            "Kindertoilettensitz in der Neustadt.",
-                            "Bild Ostsee Blick auf Hiddensee , Leinwand",
-                            "RESERVIERT Neu, zu verschenken im Schnoor",
-                            "Hat jemand verwendung für filmdosen? In der Neustadt RESERVIERT",
-                            "A3 Zeichenblöcke zu verschenken - liegen draußen in der hemelinger Straße vor nr 68",
-                            "Kinderteller zu verschenkeb",
-                            "no description",
-                            "kinder neustadt",
-                            "eine Zauberbox (ab 6 Jahren, vollständig, gut erhalten",
-                            "Utrusta backofen halterung (neu, wurde falsch geliefert)",
-                            "Wanduhr.... funktioniert leider nicht....",
-                            "Bremen Schwachhausen....",
-                            "Schwachhausen....",
-                            "Silikon Backform Reserviert",
-                            "Spiegel Massiv Eisen Durchmesser: 62 cm",
-                            "no description",
-                            "Sprechblase die mit einem abwischbaren Stift (whiteboard maker) beschriftet werden kann. Den Stift hab ich nicht mehr",
-                            "Große haarklammer und zopfband im Viertel RESERVIERT",
-                            "Jemand interessiert? Kann CD, USB und SD Karte. Kabel fehlt. Ist aber auch mit Batterien zu betreiben. Weiß leider nicht, ob der noch geht.",
-                            "Kerzenständer zu verschenken in Mitte. - reserviert",
-                            "Zwei einwandfreie Stühle. Abzuholen in der Neustadt! Reserviert",
-                            "In der Neustadt",
-                            "Mandoline (?) in der Neustadt reserviert",
-                            "description lost",
-                            "description lost",
-                            "Selbstverständlich unbenutzt. Nur ein mal geöffnet, um zu prüfen, ob dieses auf meine Maschine passt. Schwachhausen",
-                            "Kratzbaumelement: Hängematte für Kitten oder kleine Katzen. Hat einen Riss, kann aber repariert werden oder neu bezogen werden. Müsste aber ein Kitten auch so aushalten :) Neustadt",
-                            "Hallo liebe Leute habe eine große halal Geflügelwurst 900g abzugeben bei der Ritterhuder Straße 1 in Gröpelingen bitte PN",
-                            "I’m Viertel",
-                            "[Reserviert] Plüschiges Sparschweinchen in Schwachhausen abzugeben.",
-                            "Buntstifte in der Bahnhofsvorstadt zu verschenken",
-                            "Stuhl abzugeben im Viertel :)",
-                            "Funktioniert. Sound halt Werbe- Geschenk-entsprechend. Schwachhausen  RESERVIERT",
-                            "Tauscht jemand diese nagelneue Gugelhupfform gegen eine Tarte-Form (kann gerne schon alt sein, mag keine neuen Sachen). PN Büdde",
-                            "no description",
-                            "Zu verschenken in sebaldsbrück",
-                            "Klo Bär 🤣 in der Neustadt" ]
-
-    for (let i = 1; i < descriptionList.length; i++) {
-        if (value == i) {
-            text += descriptionList[i];
-        }
-    }
-    
-    description.text(text);
-}
 
 $("[data-id]").each(function(element) {
     $(this).click(function () {
-        changeText($(this).data("id"))
+        $("#description").text(descriptionList[$(this).data("id")]);
     })
     $(this).draggable()
 })
