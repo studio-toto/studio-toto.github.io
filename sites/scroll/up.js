04a3e9bfbbb71a3753dd38b5a51cd951c95aa36d
juliavollmer <julia.vollmer06@web.de>
2021-06-07 21:02:37 +0200

    replaced scrollbar with fake one

--- sites/scroll/up.js
+++ sites/scroll/up.js
@@ -3,3 +3,24 @@ function scrollToBottom() {
 }
 history.scrollRestoration = "manual";
 window.onload = scrollToBottom;
+var divscroll = document.body;
+$("#scrollimage").draggable({ axis: "y" });
+
+function myFunction() {
+    var ratio = divscroll.scrollTop/divscroll.scrollHeight*100;
+    console.log(ratio);
+    // if (divscroll.scrollTop > 4400) {
+    //     $('#scrollimage').css({'bottom': '0'});
+    // }
+    // else 
+    if (ratio > 3) {
+        $('#scrollimage').css({'bottom': 100-12-ratio + '%'});
+    } else {
+        $('#scrollimage').css({'bottom': '85vh'});
+    }
+console.log(divscroll.scrollTop);
+}
+
+$('body').bind('scroll', function() {
+    myFunction();
+ }); 
