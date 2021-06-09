e67f44fb962d850a873a1ec6725ce52f0099a853
juliavollmer <julia.vollmer06@web.de>
2021-06-09 12:46:57 +0200

    scroll update + scroll test

--- sites/scroll/up.js
+++ sites/scroll/up.js
@@ -6,21 +6,33 @@ window.onload = scrollToBottom;
 var divscroll = document.body;
 $("#scrollimage").draggable({ axis: "y" });
 
-function myFunction() {
+function scrollFunction() {
     var ratio = divscroll.scrollTop/divscroll.scrollHeight*100;
-    console.log(ratio);
-    // if (divscroll.scrollTop > 4400) {
-    //     $('#scrollimage').css({'bottom': '0'});
-    // }
-    // else 
-    if (ratio > 3) {
-        $('#scrollimage').css({'bottom': 100-12-ratio + '%'});
-    } else {
-        $('#scrollimage').css({'bottom': '85vh'});
-    }
-console.log(divscroll.scrollTop);
+    var directionsc = "bottom";
+    
+        $('.scrollimage').each(function(element) {
+            directionsc = $(this).attr("data-scroll");
+           
+            if (ratio > 1) {
+                if (directionsc === "right" || directionsc === "left") {
+                    var wratio = ratio*window.outerWidth/1225;
+                    console.log(wratio);
+                    $(this).css(directionsc, 100-7.5-wratio + '%');
+                } else {
+                    $(this).css(directionsc, 100-12-ratio + '%');
+                }
+        } else {
+            if (directionsc === "right" || directionsc === "left") {
+                $(this).css(directionsc, '92.5vw');
+            } else {
+                $(this).css(directionsc, '85vh');
+            }
+        }
+          });
+        // $('.scrollimage').css({'bottom': 100-12-ratio + '%'});
+   
 }
 
 $('body').bind('scroll', function() {
-    myFunction();
+    scrollFunction();
  }); 
