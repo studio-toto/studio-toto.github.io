c0707e91529db1e6a76da0cfac7dc48daae798b0
juliavollmer <julia.vollmer06@web.de>
2021-06-27 20:38:16 +0200

    karaoke lyrics fixed so not showing at empty sections

--- sites/karaoke/karaoke.js
+++ sites/karaoke/karaoke.js
@@ -76,39 +76,16 @@ audio.addEventListener('playing',function() {  for (i = 0; i < sentences.length;
   } },false);
  
   function getSentence() {
+      let hasLyrics = false;  //check if no sentence should be displayed
     for (i = 0; i < sentences.length; i++) { //for all sentences
         if (sentences[i].start <= audio.currentTime && audio.currentTime <= sentences[i].end){
             console.log(sentences[i].sentence);
             document.getElementById("lyrics").innerHTML = sentences[i].sentence;
+            hasLyrics = true;
         }
+      }
+      if (hasLyrics== false){
+        document.getElementById("lyrics").innerHTML = "";
       }
   }
   setInterval(getSentence, 100);
-
-//1. approach
-
-//store the sentences in some list, array or object
-// var sentences = [{"sentence": "when I look back upon my life", "start": 43.900, "end": 47.900},
-// {"sentence": "it's always with a sense of shame", "start": 48, "end": 41.300}];
-
-
-// //check the audio currentTime with the lists start and end time
-// for (i = 0; i < sentences.length; i++) { //for all sentences
-//     console.log(sentences[i].sentence);
-//     if (sentences[i].start <= audio.currentTime && audio.currentTime <= sentences[i].end){
-//         console.log(sentences[i].sentence);
-//     }
-//   }
-
-//   //2.approach
-// var start, end;
-
-// //add the sentences in the html file with the same class and give them the data attributes data-start and data-end, etc.
-
-//   $('.lyrics').each(function(element) {
-//     start = $(this).attr("data-start");
-//     end = $(this).attr("data-end");
-//     if (start <= audio.currentTime && audio.currentTime <= end){
-//         //when audio time is same as subtitle do this
-//     }
-//   });
