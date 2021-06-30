8ed8f3edd70df67a7a6886d68d6baea47bf499e3
lvitters <luvit023@gmail.com>
2021-06-30 18:08:08 +0200

    no three js it doesnt work everything is terrible I hate my life

--- sites/gooey/js/scene.js
+++
@@ -1,8 +0,0 @@
-console.log(document.documentElement.scrollHeight);
-
-const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, document.documentElement.scrollHeight / 2, document.documentElement.scrollHeight / - 2, 1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, document.documentElement.scrollHeight );
-document.body.appendChild( renderer.domElement );
