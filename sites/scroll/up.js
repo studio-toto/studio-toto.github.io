afcb7c47d9ee08f88588f3665e85ce79ed3cbccf
juliavollmer <julia.vollmer06@web.de>
2021-10-18 20:25:52 +0200

    changed scroll sites

--- sites/scroll/up.js
+++ sites/scroll/up.js
@@ -1,47 +1,76 @@
 function scrollToBottom() {
     document.body.scrollTo(0, document.body.scrollHeight);
+
 }
 
-
-history.scrollRestoration = "manual";
-window.onload = scrollToBottom;
-var divscroll = document.body;
-$("#scrollimage").draggable({
-    axis: "y"
-});
-
-function scrollFunction() {
-
-    var ratio = divscroll.scrollTop / divscroll.scrollHeight * 100;
-    var directionsc = "bottom";
-
-    $('.scrollimage').each(function (element) {
-        directionsc = $(this).attr("data-scroll");
-
-        if (ratio > 1) {
-            if (directionsc === "right" || directionsc === "left") {
-                var wratio = ratio * window.outerWidth / 1225;
-                console.log(wratio);
-                $(this).css(directionsc, 100 - 7.5 - wratio + '%');
-            } else {
-                $(this).css(directionsc, 100 - 12 - ratio + '%');
-            }
-        } else {
-            if (directionsc === "right" || directionsc === "left") {
-                $(this).css(directionsc, '92.5vw');
-            } else {
-                $(this).css(directionsc, '85vh');
-            }
+$(window).ready(function() {
+    history.scrollRestoration = "manual";
+    window.onload = scrollToBottom;
+    var divscroll = document.body;
+    // Function to imitate draggable scroll function
+    $("#scrollimage").draggable({
+        axis: "y",
+        cursor: "s-resize",
+        containment: "window",
+        start: function () {
+            elDrag = $(this);     
+           elDrag.css({'left': 'auto', 'right': '0' });
+            $('.scrollbutton').css('animation-play-state', 'running');
+        },         
+        drag: function() {
+            var offset = elDrag.position();
+           
+            var yPos = offset.top/window.innerHeight*divscroll.scrollHeight;
+            console.log(yPos);
+            divscroll.scrollTo({
+                top: yPos,
+                left: 0,
+                behavior: 'auto'
+              });
+              elDrag.css({'left': 'auto', 'right': '0' });
+        },
+        stop: function(){
+            
+           elDrag.css({'left': 'auto', 'right': '0' });
+            $('.scrollbutton').css('animation-play-state', 'pause');
         }
     });
-    // $('.scrollimage').css({'bottom': 100-12-ratio + '%'});
+    function scrollFunction() {
 
-}
+        var ratio = divscroll.scrollTop / divscroll.scrollHeight * 95;
+        console.log(divscroll.scrollTop);
+        var directionsc = "bottom";
+    
+        // $('.scrollbutton').each(function (index, element) {
+        //     directionsc = $(this).attr("data-scroll");
+    
+        //     if (ratio > 1) {
+        //         if (directionsc === "right" || directionsc === "left") {
+        //             var wratio = ratio * window.outerWidth / 1225;
+        //             console.log(wratio);
+        //             $(this).css(directionsc, 100 - 7.5 - wratio + '%');
+        //         } else {
+        //             $(this).css(directionsc, 100 - 12 - ratio + '%');
+        //         }
+        //     } else {
+        //         if (directionsc === "right" || directionsc === "left") {
+        //             $(this).css(directionsc, '92.5vw');
+        //         } else {
+        //             $(this).css(directionsc, '85vh');
+        //         }
+        //     }
+        // });
+         $('.scrollbutton').css({'top': ratio + '%'});
+         console.log(ratio);
+    
+    }
+
+
 var isScrolling;
 
-$('body').bind('scroll', function () {
+$('body').bind('wheel', function () {
 
-    $('.scrollimage').css('animation-play-state', 'running');
+    $('.scrollbutton').css('animation-play-state', 'running');
     scrollFunction();
     // Clear our timeout throughout the scroll          https://gomakethings.com/detecting-when-a-visitor-has-stopped-scrolling-with-vanilla-javascript/
     window.clearTimeout(isScrolling);
@@ -50,7 +79,7 @@ $('body').bind('scroll', function () {
     isScrolling = setTimeout(function () {
 
         // Run the callback
-        $('.scrollimage').css('animation-play-state', 'paused');
+        $('.scrollbutton').css('animation-play-state', 'paused');
 
     }, 66);
-});
+});});
